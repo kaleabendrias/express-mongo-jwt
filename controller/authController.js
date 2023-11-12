421d9b0019bc75470ca288e9324fd52d9dbf0d6b
kaleabendrias <kaleabendrias1212@gmail.com>
2023-11-12 17:15:33 +0300

    repair

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,5 @@
+const User = require('../models/user')
+
 module.exports.signup_get = (req, res) => {
     res.render('signup')
 }
@@ -6,16 +8,23 @@ module.exports.signin_get = (req, res) => {
     res.render('signin')
 }
 
-module.exports.signin_post = (req, res) => {
-    console.log(req.body)
-    const {email, password} = req.body;
-    console.log(email, password)
-    res.send('sign in post')
+module.exports.signin_post = async (req, res) => {
+    const { email, password } = req.body;
+
+    console.log(email, password);
+    res.send('user login');
 }
 
-module.exports.signup_post = (req, res) => {
-    console.log(req.body)
-    const {email, password} = req.body;
-    console.log(email, password)
-    res.send('signup post');
+module.exports.signup_post = async (req, res) => {
+   const { email, password } = req.body;
+
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  }
+  catch(err) {
+    console.log(err);
+    res.status(400).send('error, user not created');
+  }
+ 
 }
